530c46201674f774363ac4f7b06b84fbe2c7a56b
20089582 <20089582@tafe.wa.edu.au>
2025-09-07 15:54:12 +0800

    feat: implemented constructors and attributes to the display and sensor classes

--- src/display.py
+++ src/display.py
@@ -1,2 +1,9 @@
 class Display:
-    pass
+    def __init__(self, id, car_park, message: str = "", is_on: bool = False):
+        self.id = id
+        self.car_park = car_park
+        self.message = message
+        self.is_on = is_on
+
+    def __str__(self):
+        return f"Display {self.id}: {self.message}"
