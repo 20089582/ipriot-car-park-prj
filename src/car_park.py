d8c5dd55b1fd6fd84f87db79e0a8ef856b888846
20089582 <20089582@tafe.wa.edu.au>
2025-09-09 14:11:05 +0800

    chore: added comments

--- src/car_park.py
+++ src/car_park.py
@@ -2,35 +2,43 @@ from sensor import Sensor
 from display import Display
 
 class CarPark:
-    def __init__(self, location, capacity, plates=None, displays=None):
+    def __init__(self, location: str, capacity: int, plates: list = None, sensor = None , displays = None):
+        '''intialises variables'''
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
+        self.sensor = sensor or []
         self.displays = displays or []
+        
     def __str__(self):
-        return f"Location: {self.location}, bay capacity: {self.capacity}"
+        '''spits out information about the carpark'''
+        return f"Location: {self.location}, Bay capacity: {self.capacity}"
     
     def register(self, component):
+        '''registers a sensor or display component'''
         if not isinstance(component, (Sensor, Display)):
             raise TypeError("Object must be a Sensor or Display")
         elif isinstance(component, Sensor):
             self.sensors.append(component)
         elif isinstance(component, Display):
             self.sensors.append(component)
-    
+
     def add_car(self, plate):
+        '''adds car and updates display'''
         self.plates.append(plate)
         self.update_displays()
-    
     def remove_car(self, plate):
+        '''removes car and updates display'''
         if plate in self.plates:
             self.plates.remove(plate)
             self.update_displays()
     @property
     def available_bays(self):
+        '''calculates avalible bays'''
         return max(0, self.capacity - len(self.plates))
     
     def update_displays(self):
+        '''sends current data to the display'''
         data = {"available_bays": self.available_bays, "temperature": 25}
         for display in self.displays:
             display.update(data)
--- src/sensor.py
+++ src/sensor.py
@@ -3,11 +3,13 @@ import random
 
 class Sensor(ABC):
     def __init__(self, id, is_active, car_park):
+        '''intialises variables'''
         self.id = id
         self.is_active = is_active
         self.car_park = car_park
 
     def __str__(self):
+        '''spits out information about the sensor'''
         status = "active" if self.is_active else "inactive"
         return f"Sensor id: {self.id}, Sensor status: {status}"
     
@@ -16,21 +18,26 @@ class Sensor(ABC):
         pass
     
     def _scan_plate(self):
+        '''creates a fake plate number'''
         return 'FAKE-' + format(random.randint(0, 999), "03d")
     
     def detect_vehicle(self):
+        '''creates a fake plate then updates car park based on the sensor used to gather the information'''
         plate = self._scan_plate()
         self.update_car_park(plate)
 
 class EntrySensor(Sensor):
     def update_car_park(self, plate):
+        '''adds the plate number to the car park'''
         self.car_park.add_car(plate)
         print(f"Incoming 🚘 vehicle detected. Plate: {plate}")
 
 class ExitSensor(Sensor):
     def update_car_park(self, plate):
+        '''removes a plate number from the car park'''        
         self.car_park.remove_car(plate)
         print(f"Outgoing 🚗 vehicle detected. Plate: {plate}")
     
     def _scan_plate(self):
+        '''a work around for cars exiting the car park'''
         return random.choice(self.car_park.plates)
