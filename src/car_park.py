f4c79396d78f29e43039825bc7f1f2a0b61be9c3
20089582 <20089582@tafe.wa.edu.au>
2025-09-09 12:19:12 +0800

    feat: implemented add/remove cars, avalible bays, update displays

--- src/car_park.py
+++ src/car_park.py
@@ -9,6 +9,7 @@ class CarPark:
         self.displays = displays or []
     def __str__(self):
         return f"Location: {self.location}, bay capacity: {self.capacity}"
+    
     def register(self, component):
         if not isinstance(component, (Sensor, Display)):
             raise TypeError("Object must be a Sensor or Display")
@@ -16,4 +17,20 @@ class CarPark:
             self.sensors.append(component)
         elif isinstance(component, Display):
             self.sensors.append(component)
-        
+    
+    def add_car(self, plate):
+        self.plates.append(plate)
+        self.update_displays()
+    
+    def remove_car(self, plate):
+        if plate in self.plates:
+            self.plates.remove(plate)
+            self.update_displays()
+    @property
+    def available_bays(self):
+        return max(0, self.capacity - len(self.plates))
+    
+    def update_displays(self):
+        data = {"available_bays": self.available_bays, "temperature": 25}
+        for display in self.displays:
+            display.update(data)
