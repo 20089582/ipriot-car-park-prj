3e40aabdc0187cdb5e4c7595cf340bd05d68752e
20089582 <20089582@tafe.wa.edu.au>
2025-09-09 18:37:21 +0800

    chore: rushed update

--- src/car_park.py
+++ src/car_park.py
@@ -3,7 +3,7 @@ from display import Display
 
 class CarPark:
     def __init__(self, location: str, capacity: int, plates: list = None, sensors: list[Sensor] = None , displays: list[Display] = None):
-        '''intialises variables'''
+        """initialises variables"""
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
@@ -11,11 +11,11 @@ class CarPark:
         self.displays = displays or []
         
     def __str__(self):
-        '''spits out information about the carpark'''
+        """spits out information about the car park"""
         return f"Location: {self.location}, Bay capacity: {self.capacity}"
     
     def register(self, component):
-        '''registers a sensor or display component'''
+        """registers a sensor or display component"""
         if not isinstance(component, (Sensor, Display)):
             raise TypeError("Object must be a Sensor or Display")
         elif isinstance(component, Sensor):
@@ -24,11 +24,11 @@ class CarPark:
             self.displays.append(component)
 
     def add_car(self, plate):
-        '''adds car and updates display'''
+        """adds car and updates display"""
         self.plates.append(plate)
         self.update_displays()
     def remove_car(self, plate):
-        '''removes car and updates display'''
+        """removes car and updates display"""
         if plate in self.plates:
             self.plates.remove(plate)
             self.update_displays()
@@ -36,11 +36,11 @@ class CarPark:
             raise ValueError(f"Plate: {plate} not found")
     @property
     def available_bays(self):
-        '''calculates avalible bays'''
+        """calculates available bays"""
         return max(0, self.capacity - len(self.plates))
     
     def update_displays(self):
-        '''sends current data to the display'''
+        """sends current data to the display"""
         data = {"available_bays": self.available_bays, "temperature": 25}
         for display in self.displays:
             display.update(data)
--- src/display.py
+++ src/display.py
@@ -1,17 +1,19 @@
 
 class Display:
     def __init__(self,id:int,message:str="",is_on:bool=False):
-        '''intialises variables'''
+        """initialises variables"""
         self.id = id
         self.message = message
         self.is_on = is_on
 
     def __str__(self):
-        '''spits out information about the display'''
+        """spits out information about the display"""
         return f"Display {self.id}: {self.message}"
     
     def update(self, data:dict):
-        '''updates the display values'''
+        """updates the display values"""
         for key, value in data.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
             print(f"{key}: {value}")
     
